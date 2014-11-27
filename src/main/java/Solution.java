4aca139022c1e7c6d9d6f4ee5a377937070828b3
szelenin <sergey.zelenin@gmail.com>
2014-11-27 23:53:27 +0200

    working solution with BFS + map/reduce

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -2,98 +2,132 @@
  * Created by sergey on 26.11.14.
  */
 
-import java.io.*;
 import java.util.*;
 
 public class Solution {
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws IOException {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        int n = 0;
-        List<Integer> array = new ArrayList<Integer>();
-        Scanner sc = new Scanner(System.in);
-        if (sc.hasNextLine()) {
-            String strValue = sc.nextLine();
-            n = Integer.parseInt(strValue);
-        }
-        while (array.size() < n && sc.hasNextLine()) {
-            array.add(Integer.parseInt(sc.nextLine()));
-        }
-        List<Integer> lis = solve(array);
-        System.out.print(lis.size());
     }
 
-    public static List<Integer> solve(List<Integer> array) {
-        if (array == null) {
-            return new ArrayList<Integer>(0);
-        }
-        if (array.size() < 2) {
-            return array;
-        }
-        ArrayList<Row> lVector = new ArrayList<Row>();
-        Row firstRow = new Row().add(array.get(0));
-        lVector.add(firstRow);
-        for (int i = 1; i < array.size(); i++) {
-            Row row = new Row(findRowInL(lVector, array.get(i), i - 1));
-            row.add(array.get(i));
-            lVector.add(row);
-        }
-        int maxSize = -1;
-        Row result = null;
-        for (Row row : lVector) {
-            if (row.size() > maxSize) {
-                maxSize = row.size();
-                result = row;
+    static String[] graph(String[] input) {
+        Map<String, Node> allNodes = new HashMap<String, Node>();
+
+        for (int i = 0; i < input.length; i++) {
+            String s = input[i];
+            boolean lastElement = i == (input.length - 1);
+            if (lastElement) {
+                return traverseGraph(allNodes, s);
+            }
+            String[] nodeFriends = s.split(":");
+            if (nodeFriends.length > 1) {
+                String nodeName = nodeFriends[0];
+                Node node = allNodes.get(nodeName);
+                if (node == null) {
+                    node = new Node(nodeName);
+                }
+                String friendsStr = nodeFriends[1];
+                String[] friends = friendsStr.split(",");
+                for (String friendName : friends) {
+                    node.addFriend(friendName);
+                }
+                allNodes.put(nodeName, node);
+            } else {
+                allNodes.put(nodeFriends[0], new Node(nodeFriends[0]));
             }
         }
-        return result.subList();
+        return null;
     }
 
-    private static Row findRowInL(ArrayList<Row> lVector, int arrayValue, int lIndex) {
-        Row result = new Row();
-        int maxRowLength = -1;
+    private static String[] traverseGraph(Map<String, Node> allNodes, String rootNode) {
+        Queue<KeyValue> mapResult = map(rootNode, allNodes);
+        ArrayList<String> result = new ArrayList<String>();
+        int currentLevel = 1;
+        List<String> childRow = new ArrayList<String>();
+        for (KeyValue entry : mapResult) {
+            if (currentLevel == entry.getLevel()) {
+                childRow.add(entry.getNode());
+            } else {
+                currentLevel++;
+                result.add(join(childRow));
+                childRow = new ArrayList<String>();
+                childRow.add(entry.getNode());
+            }
+        }
+        result.add(join(childRow));
+        return result.toArray(new String[result.size()]);
+    }
 
-        for (int i = lIndex; i >= 0; i--) {
-            Row row = lVector.get(i);
-            int lastRowElement = row.lastElement();
-            if (lastRowElement < arrayValue && maxRowLength < row.size()) {
-                result = row;
-                maxRowLength = row.size();
+    private static Queue<KeyValue> map(String rootNode, Map<String, Node> allNodes) {
+        Set<String> traversed = new HashSet<String>();
+        PriorityQueue<KeyValue> mapResult = new PriorityQueue<KeyValue>();
+        Queue<String> queue = new LinkedList<String>();
+        queue.add(rootNode);
+        int level = 1;
+        while (!queue.isEmpty()) {
+            Node node = allNodes.get(queue.poll());
+            if (node == null) {
+                continue;
+            }
+            traversed.add(node.name);
+            for (String friend : node.friends) {
+                if (traversed.contains(friend)) {
+                    continue;
+                }
+                mapResult.add(new KeyValue(level, friend));
+                queue.add(friend);
+                traversed.add(friend);
+            }
+            level++;
+        }
+        return mapResult;
+    }
+
+    private static String join(List<String> friends) {
+        String result = "";
+        for (int i = 0; i < friends.size(); i++) {
+            String node = friends.get(i);
+            result += node;
+            if (i < friends.size() - 1) {
+                result += ",";
             }
         }
         return result;
     }
 
-    private static class Row {
-        private ArrayList<Integer> row;
-        private int lastIndex;
+    static class Node {
+        private String name;
+        private List<String> friends = new ArrayList<String>();
 
-        public Row(Row other) {
-            this.row = other.row;
-            this.lastIndex = other.size();
+        public Node(String name) {
+            this.name = name;
         }
 
-        public Row() {
-            row = new ArrayList<Integer>();
-            lastIndex = -1;
+        public void addFriend(String friend) {
+            friends.add(friend);
+        }
+    }
+
+    static class KeyValue implements Comparable<KeyValue> {
+        private int level;
+        private String node;
+
+        @Override
+        public int compareTo(KeyValue o) {
+            return level - o.level;
         }
 
-        public int lastElement() {
-            return row.get(lastIndex);
+        KeyValue(int level, String node) {
+            this.level = level;
+            this.node = node;
         }
 
-        public int size() {
-            return lastIndex + 1;
+        public int getLevel() {
+            return level;
         }
 
-        public Row add(Integer element) {
-            row.add(element);
-            lastIndex = row.size() - 1;
-            return this;
-        }
-
-        public List<Integer> subList() {
-            return row.subList(0, lastIndex);
+        public String getNode() {
+            return node;
         }
     }
 }
+
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -6,14 +6,9 @@ import static org.junit.Assert.*;
 
 public class SolutionTest {
     @Test
-    public void shouldFindLis() {
-        assertEquals(Arrays.asList(1), Solution.solve(Arrays.asList(1)));
-    }
-
-    @Test
     public void should() {
-        assertEquals(Arrays.asList(15, 27, 38, 55, 65, 85),
-                Solution.solve(Arrays.asList(15, 27, 14, 38, 26, 55, 46, 65, 85)));
+        assertArrayEquals(new String[]{"B,C,D"}, Solution.graph(new String[]{"A:B,C,D", "A"}));
+        assertArrayEquals(new String[]{"B,C,D", "E"}, Solution.graph(new String[]{"A:B,C,D", "B:A,D,E", "C:E,B", "A"}));
     }
 
 }
