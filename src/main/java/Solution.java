95567fd80dc31ce8a3539a5338f73ba19a818b92
szelenin <sergey.zelenin@gmail.com>
2014-11-27 08:42:12 +0200

    not working solution

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -4,9 +4,6 @@
 
 import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class Solution {
 
@@ -33,38 +30,70 @@ public class Solution {
         if (array.size() < 2) {
             return array;
         }
-        ArrayList<ArrayList<Integer>> lVector = new ArrayList<ArrayList<Integer>>();
-        ArrayList<Integer> firstRow = new ArrayList<Integer>(1);
-        firstRow.add(array.get(0));
+        ArrayList<Row> lVector = new ArrayList<Row>();
+        Row firstRow = new Row().add(array.get(0));
         lVector.add(firstRow);
         for (int i = 1; i < array.size(); i++) {
-            ArrayList<Integer> row = new ArrayList<Integer>(findRowInL(lVector, array.get(i), i - 1));
+            Row row = new Row(findRowInL(lVector, array.get(i), i - 1));
             row.add(array.get(i));
             lVector.add(row);
         }
         int maxSize = -1;
-        ArrayList<Integer> result = new ArrayList<Integer>();
-        for (ArrayList<Integer> row : lVector) {
+        Row result = null;
+        for (Row row : lVector) {
             if (row.size() > maxSize) {
                 maxSize = row.size();
                 result = row;
             }
         }
-        return result;
+        return result.subList();
     }
 
-    private static ArrayList<Integer> findRowInL(ArrayList<ArrayList<Integer>> lVector, int arrayValue, int lIndex) {
-        ArrayList<Integer> result = new ArrayList<Integer>(0);
+    private static Row findRowInL(ArrayList<Row> lVector, int arrayValue, int lIndex) {
+        Row result = new Row();
         int maxRowLength = -1;
 
         for (int i = lIndex; i >= 0; i--) {
-            ArrayList<Integer> row = lVector.get(i);
-            Integer lastRowElement = row.get(row.size() - 1);
+            Row row = lVector.get(i);
+            int lastRowElement = row.lastElement();
             if (lastRowElement < arrayValue && maxRowLength < row.size()) {
                 result = row;
                 maxRowLength = row.size();
             }
         }
         return result;
+    }
+
+    private static class Row {
+        private ArrayList<Integer> row;
+        private int lastIndex;
+
+        public Row(Row other) {
+            this.row = other.row;
+            this.lastIndex = other.size();
+        }
+
+        public Row() {
+            row = new ArrayList<Integer>();
+            lastIndex = -1;
+        }
+
+        public int lastElement() {
+            return row.get(lastIndex);
+        }
+
+        public int size() {
+            return lastIndex + 1;
+        }
+
+        public Row add(Integer element) {
+            row.add(element);
+            lastIndex = row.size() - 1;
+            return this;
+        }
+
+        public List<Integer> subList() {
+            return row.subList(0, lastIndex);
+        }
     }
 }
